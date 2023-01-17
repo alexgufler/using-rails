2c0d2e0c3ed1cf8e370a41413c02b8f375d251da
Alex Gufler <a.gufler@live.de>
2023-01-17 16:41:19 +0100

    create the first route, controller action, model/migration and view to display all articles

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "articles#index"
+
+  # map GET /articles requests to the index action of ArticlesController
+  get "/articles", to: "articles#index" 
 end
